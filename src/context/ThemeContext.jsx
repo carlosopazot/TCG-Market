c9d5267ee7302404a84c41f206e0c3e80cb2e5a9
Carlos Opazo <carlosopazo@MacBook-Pro-de-Carlos.local>
2024-01-23 01:23:32 -0300

    :lipstick: minor fixes

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,19 +1,22 @@
-import { useState, createContext } from "react";
-import { ConfigProvider, theme } from "antd";
+import { useState, createContext } from "react"
+import { ConfigProvider, theme } from "antd"
 
 export const ThemeContext =createContext()
 
 export const ThemeProvider = ({ children }) => {
-  const { defaultAlgorithm, darkAlgorithm } = theme;
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const { defaultAlgorithm, darkAlgorithm } = theme
+  const [isDarkMode, setIsDarkMode] = useState(false)
   const toggleDarkMode = () => {
-    setIsDarkMode((previousValue) => !previousValue);
-   };
+    setIsDarkMode((previousValue) => !previousValue)
+   }
   return(
     <ThemeContext.Provider value={{ toggleDarkMode, isDarkMode, setIsDarkMode }}>
       <ConfigProvider
         theme={{
           algorithm: isDarkMode ? darkAlgorithm : defaultAlgorithm,
+          token: {
+            fontFamily: 'DM Sans'
+          }
         }}
       >
       {children}
